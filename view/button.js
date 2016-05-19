5b8b9bfc4e65e5c452fc28c5489ab318b5cc8c2b
Alexey Litvinov <sullenor@yandex-team.ru>
2016-05-19 14:53:55 +0300

    button proptypes update

--- view/button.js
+++ view/button.js
@@ -13,9 +13,37 @@ Button.defaultProps = {
 };
 
 Button.propTypes = {
+  onBlur: PropTypes.func,
   onClick: PropTypes.func,
+  onContextMenu: PropTypes.func,
+  onDoubleClick: PropTypes.func,
+  onDrag: PropTypes.func,
+  onDragEnd: PropTypes.func,
+  onDragEnter: PropTypes.func,
+  onDragExit: PropTypes.func,
+  onDragLeave: PropTypes.func,
+  onDragOver: PropTypes.func,
+  onDragStart: PropTypes.func,
+  onDrop: PropTypes.func,
+  onFocus: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  onKeyPress: PropTypes.func,
+  onKeyUp: PropTypes.func,
+  onMouseDown: PropTypes.func,
+  onMouseEnter: PropTypes.func,
+  onMouseLeave: PropTypes.func,
+  onMouseMove: PropTypes.func,
+  onMouseOut: PropTypes.func,
+  onMouseOver: PropTypes.func,
+  onMouseUp: PropTypes.func,
+  onTouchCancel: PropTypes.func,
+  onTouchEnd: PropTypes.func,
+  onTouchMove: PropTypes.func,
+  onTouchStart: PropTypes.func,
+  onTransitionEnd: PropTypes.func,
   styles: PropTypes.object,
 };
+
 
 module.exports = cssModules(Button);
 module.exports.Button = createTag(module.exports);
