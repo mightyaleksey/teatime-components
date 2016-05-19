ca4f88878412fcecef3ae6d236f78cd93b89d940
Alexey Litvinov <sullenor@yandex-team.ru>
2016-05-19 16:15:49 +0300

    input component

---
+++ component/input.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const { Input: BaseInput } = require('../view/input');
+const { PropTypes } = require('react');
+const { createTag } = require('../');
+
+const styles = {
+  's': require('../style/input/input-s.css'),
+  'm': require('../style/input/input-m.css'),
+  'l': require('../style/input/input-l.css'),
+};
+
+function Input({ size, ...o }) {
+  return BaseInput({...o, styles: styles[size]});
+}
+
+Input.defaultProps = {
+  size: 'm',
+};
+
+Input.propTypes = {
+  defaultValue: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  onCompositionEnd: PropTypes.func,
+  onCompositionStart: PropTypes.func,
+  onCompositionUpdate: PropTypes.func,
+  onCopy: PropTypes.func,
+  onCut: PropTypes.func,
+  onFocus: PropTypes.func,
+  onInput: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  onKeyPress: PropTypes.func,
+  onKeyUp: PropTypes.func,
+  onPaste: PropTypes.func,
+  onSelect: PropTypes.func,
+  onSubmit: PropTypes.func,
+  placeholder: PropTypes.string,
+  size: PropTypes.oneOf([
+    's',
+    'm',
+    'l',
+  ]),
+  value: PropTypes.string,
+};
+
+module.exports = Input;
+module.exports.Input = createTag(module.exports);
---
+++ view/input.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const { PropTypes } = require('react');
+const { createTag, input } = require('../');
+const cssModules = require('react-css-modules');
+
+function Input(props) {
+  return input({...props, styleName: 'control'});
+}
+
+Input.defaultProps = {
+  styles: {},
+};
+
+Input.propTypes = {
+  defaultValue: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  onCompositionEnd: PropTypes.func,
+  onCompositionStart: PropTypes.func,
+  onCompositionUpdate: PropTypes.func,
+  onCopy: PropTypes.func,
+  onCut: PropTypes.func,
+  onFocus: PropTypes.func,
+  onInput: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  onKeyPress: PropTypes.func,
+  onKeyUp: PropTypes.func,
+  onPaste: PropTypes.func,
+  onSelect: PropTypes.func,
+  onSubmit: PropTypes.func,
+  placeholder: PropTypes.string,
+  styles: PropTypes.object,
+  value: PropTypes.string,
+};
+
+module.exports = cssModules(Input);
+module.exports.Input = createTag(module.exports);
