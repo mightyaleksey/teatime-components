b94039ea69ce207d3dc4c94ccfda06b7df25360b
Alexey Litvinov <sullenor@yandex-team.ru>
2016-05-26 10:27:12 +0300

    jsdoc added

--- mixin/simplify.js
+++ mixin/simplify.js
@@ -3,6 +3,14 @@
 const React = require('react');
 const { Component } = React;
 
+/**
+ * @param  {component} Target
+ * @param  {function} getter
+ * @param  {object} styles
+ * @param  {object} defaults defaultProps
+ * @param  {object} types propTypes
+ * @return {component}
+ */
 module.exports = function simplify(Target, getter, styles, defaults, types) {
   class Simplified extends Component {
     render() {
