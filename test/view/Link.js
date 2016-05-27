259db570f91d053ec0c43facb97ea61a22fab35d
Alexey Litvinov <sullenor@yandex-team.ru>
2016-05-27 20:20:27 +0300

    test for link and textarea

---
+++ test/view/Link.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const Link = require('../../view/Link');
+const React = require('react');
+const { shallowRender } = require('skin-deep');
+const test = require('tape');
+
+const styles = {control: 'control'};
+
+test('className to contain `mixin` and styleName', t => {
+  const tree = shallowRender(<Link
+    className='mixin'
+    href='#'
+    styles={styles}/>);
+
+  const result = tree.getRenderOutput();
+
+  t.isEqual(result.props.className, 'mixin control');
+  t.end();
+});
---
+++ test/view/Textarea.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const React = require('react');
+const Textarea = require('../../view/Textarea');
+const { shallowRender } = require('skin-deep');
+const test = require('tape');
+
+const styles = {control: 'control'};
+
+test('className to contain `mixin` and styleName', t => {
+  const tree = shallowRender(<Textarea
+    className='mixin'
+    name='motorrad'
+    styles={styles}/>);
+
+  const result = tree.getRenderOutput();
+
+  t.isEqual(result.props.className, 'mixin control');
+  t.end();
+});
