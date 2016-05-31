06d1052bb9f02f555158d2dbd69ad1807feb9f44
Alexey Litvinov <sullenor@yandex-team.ru>
2016-05-31 15:32:14 +0300

    test for mixin

---
+++ test/view/ColorPicker.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const ColorPicker = require('../../view/ColorPicker');
+const React = require('react');
+const { shallowRender } = require('skin-deep');
+const test = require('tape');
+
+const styles = {
+  item: 'item',
+  line: 'line',
+  menu: 'menu',
+  preview: 'preview',
+  wrapper: 'wrapper',
+};
+
+test('className to contain `mixin` and styleName', t => {
+  const tree = shallowRender(<ColorPicker
+    className='mixin'
+    name='motorrad'
+    styles={styles}
+    value='369'/>);
+
+  const result = tree.dive(['ColorPicker']).getRenderOutput();
+
+  t.isEqual(result.props.className, 'mixin wrapper');
+  t.end();
+});
