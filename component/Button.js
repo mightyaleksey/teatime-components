115c41b2076e31f3269d9b8d532e080a0ea6f9ca
Alexey Litvinov <sullenor@yandex-team.ru>
2016-06-07 17:50:15 +0300

    button styled as link implementation

--- component/Button.js
+++ component/Button.js
@@ -12,6 +12,10 @@ module.exports = simplify(
     'action-s': require('../style/button/button-action-s.css'),
     'action-m': require('../style/button/button-action-m.css'),
     'action-l': require('../style/button/button-action-l.css'),
+    'link-xs': require('../style/button/button-link-xs.css'),
+    'link-s': require('../style/button/button-link-s.css'),
+    'link-m': require('../style/button/button-link-m.css'),
+    'link-l': require('../style/button/button-link-l.css'),
     'normal-xs': require('../style/button/button-normal-xs.css'),
     'normal-s': require('../style/button/button-normal-s.css'),
     'normal-m': require('../style/button/button-normal-m.css'),
@@ -30,6 +34,7 @@ module.exports = simplify(
     ]),
     theme: PropTypes.oneOf([
       'action',
+      'link',
       'normal',
     ]),
   }
