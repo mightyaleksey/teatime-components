73ccabeb50f59c5b2d61dbb4ff7625cec064083b
Alexey Litvinov <sullenor@yandex-team.ru>
2016-06-24 17:15:24 +0300

    select2 -> select + maxHeight for menu

--- view/Select.js
+++ view/Select.js
@@ -3,13 +3,14 @@
 const { Component, PropTypes } = require('react');
 const { bind, hasValueProp, indexOf } = require('../tool/component');
 const { classNames, composition } = require('../tool/className');
+const { constant } = require('../tool/func');
 const { findDOMNode } = require('react-dom');
 const { generateId, hasUniqueValues, mapKey, mapKeyBasedOnPos } = require('../tool/identity');
 const { isUndefined, noop } = require('../tool/func');
-const Button = require('./Button');
 const Option = require('./Option');
 const Overlay = require('./Overlay');
 const React = require('react');
+const fuzzysearch = require('fuzzysearch');
 const reactOutsideEvent = require('../mixin/ReactOutsideEvent');
 const warning = require('../tool/warning');
 
@@ -20,6 +21,9 @@ class Select extends Component {
     super(props);
 
     bind(this, [
+      'onControlBlur',
+      'onControlFocus',
+      'onInputChange',
       'onKeyDown',
       'onMenuToggle',
       'onOptionFocus',
@@ -51,10 +55,27 @@ class Select extends Component {
 
     this.state = {
       focused: -1,
+      inFocus: false,
+      inputValue: '',
       isOpened: false,
       prefix: generateId(),
       selected,
     };
+  }
+
+  closeMenu() {
+    this.setState({
+      isOpened: false,
+      focused: -1,
+      inputValue: '',
+    });
+  }
+
+  openMenu() {
+    this.setState({
+      isOpened: true,
+      focused: this.state.selected,
+    });
   }
 
   componentDidUpdate() {
@@ -93,20 +114,6 @@ class Select extends Component {
     }
   }
 
-  closeMenu() {
-    this.setState({
-      isOpened: false,
-      focused: -1,
-    });
-  }
-
-  openMenu() {
-    this.setState({
-      isOpened: true,
-      focused: this.state.selected,
-    });
-  }
-
   focus() {
     if (this.refs.label) {
       this.refs.label.focus();
@@ -130,6 +137,15 @@ class Select extends Component {
   }
 
   /**
+   * @param  {object} option
+   * @param  {string} option.label
+   * @return {string}
+   */
+  getHaystack(option) {
+    return option.label;
+  }
+
+  /**
    * @param  {number} selected
    * @return {string}
    */
@@ -147,6 +163,25 @@ class Select extends Component {
     return selected !== -1
       ? this.props.options[selected].value
       : '';
+  }
+
+  onControlBlur() {}
+
+  onControlFocus() {
+    if (this._openAfterFocus !== false) {
+      return void this.setState({
+        isOpened: true,
+      });
+    }
+
+    this.__openAfterFocus = true;
+  }
+
+  onInputChange(e) {
+    this.setState({
+      inputValue: e.target.value,
+      isOpened: true,
+    });
   }
 
   onKeyDown(e) {
@@ -172,6 +207,8 @@ class Select extends Component {
       break;
 
     case 32: // space
+      if (this.props.isSearchable) return;
+
       if (!isOpened) {
         this.openMenu();
       } else {
@@ -213,38 +250,31 @@ class Select extends Component {
     return void this.openMenu();
   }
 
+  /**
+   * @param {object} e
+   * @param {*} _
+   * @param {number} tc
+   */
   onOptionFocus(e, _, tc) {
     if (this.state.focused === tc) return;
     this.setState({focused: tc});
   }
 
+  /**
+   * @param {object} e
+   * @param {*} _
+   * @param {number} tc
+   */
   onOptionSelect(e, _, tc) {
+    this._openAfterFocus = false;
     this.updateValue(e, tc);
     this.focus();
   }
 
   onOutsideEvent() {
+    this._openAfterFocus = true;
     if (!this.state.isOpened) return;
     this.closeMenu();
-  }
-
-  /**
-   * @param  {object} e
-   * @param  {number} nextSelected
-   */
-  updateValue(e, nextSelected) {
-    const nextState = {isOpened: false};
-
-    if (nextSelected === -1 || nextSelected === this.state.selected) {
-      return void this.setState(nextState);
-    }
-
-    if (!this.controlled) {
-      nextState.selected = nextSelected;
-    }
-
-    this.setState(nextState);
-    this.props.onChange(e, {value: this.props.options[nextSelected].value});
   }
 
   /**
@@ -257,46 +287,89 @@ class Select extends Component {
       : mapKey;
   }
 
+  /**
+   * @param {object} e
+   * @param {number} nextSelected
+   */
+  updateValue(e, nextSelected) {
+    const nextState = {isOpened: false};
+
+    if (nextSelected === -1 || nextSelected === this.state.selected) {
+      return void this.setState(nextState);
+    }
+
+    if (!this.controlled) {
+      nextState.selected = nextSelected;
+    }
+
+    if (this.props.isSearchable) {
+      nextState.inputValue = '';
+    }
+
+    this.setState(nextState);
+    this.props.onChange(e, {value: this.props.options[nextSelected].value});
+  }
+
   render() {
     return (
-      <div
-        {...this.props}
-        className={composition(this.props)}
-        onKeyDown={this.onKeyDown}>
+      <div className={composition(this.props)}>
         {this.renderLabel()}
         {this.renderMenu()}
-        <input
-          disabled={this.props.disabled}
-          name={this.props.name}
-          type='hidden'
-          value={this.getSelectedValue(this.state.selected)}/>
+        {this.renderValue()}
       </div>
     );
   }
 
   renderLabel() {
+    const { styles } = this.props;
+
+    if (!this.props.isSearchable) {
+      return (
+        <button
+          className={classNames(styles.control, {
+            [styles.isClosedControl]: !this.state.isOpened,
+            [styles.isOpenedControl]: this.state.isOpened,
+          })}
+          onClick={this.onMenuToggle}
+          onKeyDown={this.onKeyDown}
+          ref='label'
+          tabIndex={this.props.tabIndex || 0}>
+          {this.getSelectedLabel(this.state.selected)}
+        </button>
+      );
+    }
+
     return (
-      <Button
-        className={this.props.styles[this.state.isOpened ? 'isOpened' : 'isClosed']}
-        disabled={this.props.disabled}
-        onClick={this.onMenuToggle}
-        ref='label'
-        styles={this.props.styles}>
-        {this.getSelectedLabel(this.state.selected)}
-      </Button>
+      <span
+        className={classNames(styles.control, {
+          [styles.isClosedControl]: !this.state.isOpened,
+          [styles.isOpenedControl]: this.state.isOpened,
+        })}>
+        {this.state.inputValue ? '' : this.getSelectedLabel(this.state.selected)}
+        <input
+          className={this.props.styles.input}
+          onBlur={this.onControlBlur}
+          onChange={this.onInputChange}
+          onFocus={this.onControlFocus}
+          onKeyDown={this.onKeyDown}
+          ref='label'
+          tabIndex={this.props.tabIndex || 0}
+          type='text'
+          value={this.state.inputValue}/>
+      </span>
     );
   }
 
   renderMenu() {
     const { styles } = this.props;
 
-    if (!this.state.isOpened) {
-      return null;
-    }
-
     return (
       <Overlay
-        className={styles.menu}
+        className={classNames(styles.menu, {
+          [styles.isFixedMenu]: this.props.isFixed,
+          [styles.isClosedMenu]: !this.state.isOpened,
+          [styles.isOpenedMenu]: this.state.isOpened,
+        })}
         ref='menu'>
         {this.renderOptions()}
       </Overlay>
@@ -316,23 +389,37 @@ class Select extends Component {
       return null;
     }
 
-    const { options, styles } = this.props;
+    const { isFixed, isSearchable, options, styles } = this.props;
     const { focused, prefix, selected } = this.state;
-    const renderOption = this.props.renderOption || this.renderOption;
+    const inputValue = this.state.inputValue.toLowerCase();
+    const filter = isSearchable
+      ? fuzzysearch
+      : constant(true);
 
-    return options.map((option, i) => {
+    const getHaystack = this.props.getHaystack || this.getHaystack;
+    const renderOption = this.props.renderOption || this.renderOption;
+    const list = [];
+
+    for (var length = options.length, i = 0; i < length; ++i) {
+      const option = options[i];
+
+      if (!filter(inputValue, getHaystack(option).toLowerCase())) {
+        continue;
+      }
+
       const isFocused = focused === i;
       const isSelected = selected === i;
       const ref = isFocused
         ? 'selected'
         : null;
 
-      return (
+      list.push((
         <Option
           {...option}
           className={classNames(styles.item, {
-            [styles.isFocused]: isFocused,
-            [styles.isSelected]: isSelected,
+            [styles.isFixedItem]: isFixed,
+            [styles.isFocusedItem]: isFocused,
+            [styles.isSelectedItem]: isSelected,
           })}
           isFocused={isFocused}
           key={this.mapKey(prefix, option.value, i)}
@@ -342,13 +429,37 @@ class Select extends Component {
           tc={i}>
           {renderOption(option)}
         </Option>
-      );
-    });
+      ));
+    }
+
+    if (isSearchable && list.length === 0) {
+      list.push((
+        <Option
+          className={styles.empty}>
+          {this.props.noResults}
+        </Option>
+      ));
+    }
+
+    return list;
+  }
+
+  renderValue() {
+    return (
+      <input
+        disabled={this.props.disabled}
+        name={this.props.name}
+        type='hidden'
+        value={this.getSelectedValue(this.state.selected)}/>
+    );
   }
 }
 
 Select.defaultProps = {
   hasUniqValues: true,
+  isFixed: true,
+  isSearchable: false,
+  noResults: 'No results found',
   onChange: noop,
   placeholder: '—',
   styleName: 'wrapper',
@@ -356,8 +467,12 @@ Select.defaultProps = {
 };
 
 Select.propTypes = {
+  getHaystack: PropTypes.func,
   hasUniqValues: PropTypes.bool,
+  isFixed: PropTypes.bool,
+  isSearchable: PropTypes.bool,
   name: PropTypes.string.isRequired,
+  noResults: PropTypes.string,
   onChange: PropTypes.func,
   options: PropTypes.array,
   placeholder: PropTypes.string,
@@ -365,10 +480,15 @@ Select.propTypes = {
   styleName: PropTypes.string,
   styles: PropTypes.shape({
     control: PropTypes.string.isRequired,
-    isClosed: PropTypes.string.isRequired,
-    isFocused: PropTypes.string.isRequired,
-    isOpened: PropTypes.string.isRequired,
-    isSelected: PropTypes.string.isRequired,
+    input: PropTypes.string.isRequired,
+    isClosedControl: PropTypes.string.isRequired,
+    isClosedMenu: PropTypes.string.isRequired,
+    isFixedItem: PropTypes.string.isRequired,
+    isFixedMenu: PropTypes.string.isRequired,
+    isFocusedItem: PropTypes.string.isRequired,
+    isOpenedControl: PropTypes.string.isRequired,
+    isOpenedMenu: PropTypes.string.isRequired,
+    isSelectedItem: PropTypes.string.isRequired,
     item: PropTypes.string.isRequired,
     menu: PropTypes.string.isRequired,
     wrapper: PropTypes.string,
