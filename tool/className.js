de2f4aad4cbb5ed0ab355d7a2fb30726a778fb71
Alexey Litvinov <sullenor@yandex-team.ru>
2016-06-27 00:24:51 +0300

    styleName added

--- tool/className.js
+++ tool/className.js
@@ -1,20 +1,20 @@
 'use strict';
 
+const { mapKeys } = require('./func');
 const classNames = require('classnames');
 
 exports.classNames = classNames;
-exports.composition = composition;
+exports.styleName = styleName;
 
 /**
  * @param  {object} props
  * @param  {string} props.className
  * @param  {string} props.styleName
  * @param  {object} props.styles
+ * @param  {object} [optional]
  * @return {string}
  */
-function composition(props) {
-  return classNames(props.className, props.styles[props.styleName]);
+function styleName(props, optional) {
+  return classNames(props.className, props.styles[props.styleName],
+    mapKeys((_, key) => props.styles[key], optional));
 }
-
-// @todo add helper to build state based on styles,
-//       like c(styles, {isOpened: true});
