a8d5be4cf052a6cde94677310f53fc1852c182f4
Alexey Litvinov <sullenor@yandex-team.ru>
2016-07-08 15:17:07 +0300

    pseudo buttons

--- component/Link.js
+++ component/Link.js
@@ -6,20 +6,35 @@ const StyleComponent = require('../mixin/StyleComponent');
 
 module.exports = StyleComponent(
   Link,
-  (styles, { size }) => ({styles: styles[size]}),
+  (styles, { size, theme }) => ({styles: styles[`${theme}-${size}`]}),
   {
-    's': require('../style/link/link-s.css'),
-    'm': require('../style/link/link-m.css'),
-    'l': require('../style/link/link-l.css'),
+    'action-xs': require('../style/link/link-action-xs.css'),
+    'action-s': require('../style/link/link-action-s.css'),
+    'action-m': require('../style/link/link-action-m.css'),
+    'action-l': require('../style/link/link-action-l.css'),
+    'link-xs': require('../style/link/link-link-xs.css'),
+    'link-s': require('../style/link/link-link-s.css'),
+    'link-m': require('../style/link/link-link-m.css'),
+    'link-l': require('../style/link/link-link-l.css'),
+    'normal-xs': require('../style/link/link-normal-xs.css'),
+    'normal-s': require('../style/link/link-normal-s.css'),
+    'normal-m': require('../style/link/link-normal-m.css'),
+    'normal-l': require('../style/link/link-normal-l.css'),
   },
   {
     size: 's',
+    theme: 'link',
   },
   {
     size: PropTypes.oneOf([
       's',
       'm',
       'l',
+    ]),
+    theme: PropTypes.oneOf([
+      'action',
+      'link',
+      'normal',
     ]),
   }
 );
