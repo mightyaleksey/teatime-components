4ceae6ad97a086792f696adec196109e4caa8231
Alexey Litvinov <iamhoneyboy@gmail.com>
2016-10-14 21:03:56 +0300

    deploy

---
+++ .script/compile.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ `git status -s|wc -l` -gt 0 ]
+then
+  # there are some changings in the staging area
+  # no continuation until everything is ready
+  echo commit changes first
+  exit 1
+fi
+
+babelify() {
+  node_modules/.bin/babel $1 --out-dir ${2:-$1} || exit $?
+}
+
+# convert scripts to es5
+babelify component
+babelify lib
+babelify view
+
+if [ "$1" != "--publish" ]
+then
+  # stop in case publish flag was not provided
+  exit
+fi
+
+# npm publish --tag beta
+npm publish
+git reset --hard
