7563d9cb6fc6cf63fdaeb9f1e507df8e905d028c
Alexey Litvinov <sullenor@yandex-team.ru>
2017-01-21 13:02:41 +0300

    added possibility to generate index of components

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+exports.Button = require('./component/Button');
+exports.Check = require('./component/Check');
+exports.CheckGroup = require('./component/CheckGroup');
+exports.ColorPicker = require('./component/ColorPicker');
+exports.Input = require('./component/Input');
+exports.Link = require('./component/Link');
+exports.Radio = require('./component/Radio');
+exports.Select = require('./component/Select');
+exports.Spin = require('./component/Spin');
+exports.Textarea = require('./component/Textarea');
+exports.Tooltip = require('./component/Tooltip');
+exports.Tumbler = require('./component/Tumbler');
