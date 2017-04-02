773c796baf723e19a5713ba4f21d6b74b5800cc7
Alexey Litvinov <sullenor@yandex-team.ru>
2017-04-02 23:06:07 +0300

    select tests up

--- test/component/Select.unit.test.js
+++ test/component/Select.unit.test.js
@@ -84,6 +84,57 @@ describe('(un)controllable behavior', () => {
   });
 });
 
+describe('prop #options', () => {
+  it('should update `selectedPosition` correctly in case #value and #options were changed', () => {
+    const component = shallow(
+      <Select
+        name='control'
+        options={[
+          {label: 'AA', value: 'aa'},
+          {label: 'BB', value: 'bb'},
+          {label: 'CC', value: 'cc'},
+          {label: 'DD', value: 'dd'},
+          {label: 'EE', value: 'ee'},
+          {label: 'FF', value: 'ff'},
+        ]}
+        value='cc'/>
+    );
+
+    component.setProps({
+      options: [
+        {label: 'DD', value: 'dd'},
+        {label: 'FF', value: 'ff'},
+        {label: 'GG', value: 'gg'},
+      ],
+      value: 'gg',
+    });
+
+    expect(component.state('selectedPosition')).toBe(2);
+  });
+
+  it('should update `selectedPosition` correctly in case `searchValue` was changed', () => {
+    const component = shallow(
+      <Select
+        name='control'
+        options={[
+          {label: 'AABB', value: 'aa'},
+          {label: 'BBCC', value: 'bb'},
+          {label: 'CCDD', value: 'cc'},
+          {label: 'DDEE', value: 'dd'},
+          {label: 'EEFF', value: 'ee'},
+          {label: 'FFGG', value: 'ff'},
+        ]}
+        value='cc'/>
+    );
+
+    component.setState({searchValue: 'd'});
+    expect(component.instance()._menuItems).toHaveLength(2);
+
+    component.setProps({value: 'dd'});
+    expect(component.state('selectedPosition')).toBe(3);
+  });
+});
+
 describe('prop #value', () => {
   it('should correctly initialize with `null` value', () => {
     const component = shallow(
