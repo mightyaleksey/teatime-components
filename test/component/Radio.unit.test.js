d198dc04dc5c65c177977f651c1aad1d88eda6be
Alexey Litvinov <sullenor@yandex-team.ru>
2017-04-02 23:41:34 +0300

    radio tests update

---
+++ test/component/Radio.unit.test.js
@@ -0,0 +1,186 @@
+'use strict';
+
+const {shallow} = require('enzyme');
+const Radio = require('../../component/Radio');
+const React = require('react');
+
+describe('(un)controllable behavior', () => {
+  it('should initialize uncontrollable if `value` is not presented', () => {
+    const component = shallow(
+      <Radio
+        defaultValue='bb'
+        name='control'
+        options={[
+          {label: 'AA', value: 'aa'},
+          {label: 'BB', value: 'bb'},
+          {label: 'CC', value: 'cc'},
+        ]}/>
+    );
+
+    expect(component.instance()._controlled).toBe(false);
+  });
+
+  it('should initialize controllable if `value` is presented', () => {
+    const component = shallow(
+      <Radio
+        name='control'
+        options={[
+          {label: 'AA', value: 'aa'},
+          {label: 'BB', value: 'bb'},
+          {label: 'CC', value: 'cc'},
+        ]}
+        value='bb'/>
+    );
+
+    expect(component.instance()._controlled).toBe(true);
+  });
+
+  it('should not update state `value` if controllable', () => {
+    const onChange = jest.fn();
+    const component = shallow(
+      <Radio
+        name='control'
+        onChange={onChange}
+        options={[
+          {label: 'AA', value: 'aa'},
+          {label: 'BB', value: 'bb'},
+          {label: 'CC', value: 'cc'},
+        ]}
+        value='bb'/>
+    );
+
+    component.instance()._onChange(null, {value: 'cc'});
+
+    expect(onChange).toHaveBeenCalled();
+    expect(component.state('value')).toBe('bb');
+  });
+
+  it('should update state `value` if uncontrollable', () => {
+    const onChange = jest.fn();
+    const component = shallow(
+      <Radio
+        defaultValue='bb'
+        name='control'
+        onChange={onChange}
+        options={[
+          {label: 'AA', value: 'aa'},
+          {label: 'BB', value: 'bb'},
+          {label: 'CC', value: 'cc'},
+        ]}/>
+    );
+
+    component.instance()._onChange(null, {value: 'cc'});
+
+    expect(onChange).toHaveBeenCalled();
+    expect(component.state('value')).toBe('cc');
+  });
+
+  it('should track transition', () => {
+    const component = shallow(
+      <Radio
+        name='control'
+        options={[
+          {label: 'AA', value: 'aa'},
+          {label: 'BB', value: 'bb'},
+          {label: 'CC', value: 'cc'},
+        ]}/>
+    );
+
+    expect(component.instance()._controlled).toBe(false);
+
+    component.setProps({value: 'bb'});
+
+    expect(component.instance()._controlled).toBe(true);
+  });
+});
+
+describe('prop #value', () => {
+  it('should correctly initialize with `null` value', () => {
+    const component = shallow(
+      <Radio
+        name='control'
+        options={[
+          {label: 'AA', value: 'aa'},
+          {label: 'BB', value: 'bb'},
+          {label: 'CC', value: 'cc'},
+        ]}
+        value={null}/>
+    );
+
+    expect(component.state()).toEqual(expect.objectContaining({
+      value: null,
+    }));
+  });
+
+  it('should correctly handle `null` value', () => {
+    const component = shallow(
+      <Radio
+        name='control'
+        options={[
+          {label: 'AA', value: 'aa'},
+          {label: 'BB', value: 'bb'},
+          {label: 'CC', value: 'cc'},
+        ]}
+        value='cc'/>
+    );
+
+    component.setProps({value: null});
+
+    expect(component.state()).toEqual(expect.objectContaining({
+      value: null,
+    }));
+  });
+});
+
+describe('onBlur', () => {
+  it('should call the handler', () => {
+    const onBlur = jest.fn();
+    const component = shallow(
+      <Radio
+        name='control'
+        onBlur={onBlur}
+        options={[]}/>
+    );
+
+    component.find('div').simulate('blur');
+    expect(onBlur).toHaveBeenCalledTimes(1);
+  });
+});
+
+describe('onChange', () => {
+  it('should call the handler', () => {
+    const onChange = jest.fn();
+    const component = shallow(
+      <Radio
+        name='control'
+        onChange={onChange}
+        options={[
+          {label: 'AA', value: 'aa'},
+          {label: 'BB', value: 'bb'},
+          {label: 'CC', value: 'cc'},
+        ]}/>
+    );
+
+    component.instance()._onChange(null, {value: 'bb'});
+    expect(onChange).toHaveBeenCalledTimes(1);
+    expect(onChange).toHaveBeenCalledWith(null, {
+      name: 'control',
+      value: 'bb',
+    });
+  });
+});
+
+describe('onFocus', () => {
+  it('should call the handler', () => {
+    const onFocus = jest.fn();
+    const component = shallow(
+      <Radio
+        name='control'
+        onFocus={onFocus}
+        options={[]}/>
+    );
+
+    component.find('div').simulate('focus');
+    expect(onFocus).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ test/component/Radio.view.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const Radio = require('../../component/Radio');
+const React = require('react');
+const renderer = require('react-test-renderer');
+
+test('defaultProp #size should have a value `m`', () => {
+  expect(Radio.defaultProps.size).toBe('m');
+});
+
+test('Renders correctly with default props', () => {
+  const tree = renderer.create(
+    <Radio
+      name='control'
+      options={[
+        {label: 'AA', value: 'aa'},
+        {label: 'BB', value: 'bb'},
+        {label: 'CC', value: 'cc'},
+      ]}/>
+  );
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('Renders correctly with defined props', () => {
+  const tree = renderer.create(
+    <Radio
+      className='outer'
+      disabled
+      id='controls-id'
+      name='control'
+      options={[
+        {label: 'AA', value: 'aa'},
+        {label: 'BB', value: 'bb'},
+        {label: 'CC', value: 'cc'},
+      ]}
+      theme='button'/>
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
